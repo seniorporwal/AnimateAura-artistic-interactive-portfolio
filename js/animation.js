c462d1d4c2ea414c783bd35b9f23f6a66100f9e7
aryan porwal <aryanporwal767@gmail.com>
2024-03-10 19:56:16 +0530

    replaced all files fro the orignal source file.

--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,24 @@
-// DEFINE DOM ELEMENTS
-
+// Define DOM elements
 const heroImage = document.querySelector("#hero__animation__img");
 
 const tl = document.querySelector("#grid__tl");
 const tr = document.querySelector("#grid__tr");
-// const bl = document.querySelector("#grid__bl");
-// const br = document.querySelector("#grid__br");
+const bl = document.querySelector("#grid__bl");
+const br = document.querySelector("#grid__br");
 
 const tlBtn = document.querySelector("#grid__tl__btn");
 const trBtn = document.querySelector("#grid__tr__btn");
-// const blBtn = document.querySelector("#grid__bl__btn");
-// const brBtn = document.querySelector("#grid__br__btn");
+const blBtn = document.querySelector("#grid__bl__btn");
+const brBtn = document.querySelector("#grid__br__btn");
 
 const tlContent = document.querySelector("#grid__tl__content");
 const trContent = document.querySelector("#grid__tr__content");
-// const blContent = document.querySelector("#grid__bl__content");
-// const brContent = document.querySelector("#grid__br__content");
+const blContent = document.querySelector("#grid__bl__content");
+const brContent = document.querySelector("#grid__br__content");
 
-// const projectOne = document.querySelector(".p-1");
-// const projectTwo = document.querySelector(".p-2");
-// const projectThree = document.querySelector(".p-3");
+const projectOne = document.querySelector(".p-1");
+const projectTwo = document.querySelector(".p-2");
+const projectThree = document.querySelector(".p-3");
 
 // Define colors and positions
 const bgColor = "var(--bg)";
@@ -27,7 +26,6 @@ const bgColorAlt = "var(--bg-alt)";
 const textColor = "var(--text)";
 const textColorAlt = "var(--text-alt)";
 
-// we're going to give these positoins to tlcontent when active and hidden
 let tlActive = "translateX(5vw) translateY(0)";
 let tlHidden = "translateX(-100vw) translateY(-100vh)";
 
@@ -40,17 +38,16 @@ let blHidden = "translateX(-100vw) translateY(100vh)";
 let brActive = "translateX(-5vw) translateY(0)";
 let brHidden = "translateX(100vw) translateY(100vh)";
 
-//Define corner that is open
+// Define corner that is open
 let activeCorner = "";
 
 // Add an event listener to the window object to listen for resize events
 window.addEventListener("resize", handleWindowResize);
 
-// Function that handles the styling when resizing window
+// Function that hanldes the styling when resizing window
 function handleWindowResize() {
   switch (activeCorner) {
     case "top-left":
-      // for mobile size
       if (window.innerWidth <= 1100) {
         tlActive = "translateX(0) translateY(0)";
         tlContent.style.transform = "translateX(0vw) translateY(0)";
@@ -63,11 +60,10 @@ function handleWindowResize() {
         tlContent.style.background = "var(--bg-transparent)";
         tlContent.style.zIndex = "200";
         tlBtn.style.zIndex = "300";
-        // trBtn.style.zIndex = "100";
-        // blBtn.style.zIndex = "100";
-        // brBtn.style.zIndex = "100";
+        trBtn.style.zIndex = "100";
+        blBtn.style.zIndex = "100";
+        brBtn.style.zIndex = "100";
       } else {
-        // for desktop screen size
         tlActive = "translateX(5vw) translateY(0)";
         tlContent.style.transform = "translateX(5vw) translateY(0)";
         tlContent.style.width = "30vw";
@@ -78,7 +74,6 @@ function handleWindowResize() {
       break;
 
     case "top-right":
-      // for mobile size
       if (window.innerWidth <= 1100) {
         trActive = "translateX(0) translateY(0)";
         trContent.style.transform = "translateX(0vw) translateY(0)";
@@ -92,10 +87,9 @@ function handleWindowResize() {
         trContent.style.zIndex = "200";
         trBtn.style.zIndex = "300";
         tlBtn.style.zIndex = "100";
-        // blBtn.style.zIndex = "100";
-        // brBtn.style.zIndex = "100";
+        blBtn.style.zIndex = "100";
+        brBtn.style.zIndex = "100";
       } else {
-        // for desktop screen size
         trActive = "translateX(-5vw) translateY(0)";
         trContent.style.transform = "translateX(-5vw) translateY(0)";
         trContent.style.width = "30vw";
@@ -105,11 +99,88 @@ function handleWindowResize() {
       }
       break;
 
-    // case "buttom-left":
-    //   break;
+    case "bottom-left":
+      if (window.innerWidth <= 600) {
+        blActive = "translateX(0) translateY(0)";
+        blContent.style.transform = "translateX(0vw) translateY(0)";
+        blContent.style.width = "100vw";
+        blContent.style.height = "100vh";
+        blContent.style.top = "0";
+        blContent.style.display = "flex";
+        blContent.style.alignItems = "center";
+        blContent.style.justifyContent = "center";
+        blContent.style.background = "var(--bg-transparent)";
+        blContent.style.zIndex = "200";
+        trBtn.style.zIndex = "100";
+        tlBtn.style.zIndex = "100";
+        blBtn.style.zIndex = "300";
+        brBtn.style.zIndex = "100";
+        projectOne.style.width = "70%";
+        projectOne.style.margin = "auto auto 0.5rem";
+        projectTwo.style.width = "70%";
+        projectTwo.style.margin = "auto auto 0.5rem";
+        projectThree.style.width = "70%";
+        projectThree.style.margin = "auto auto 0.5rem";
+      } else if (window.innerWidth <= 1100) {
+        blActive = "translateX(0) translateY(0)";
+        blContent.style.transform = "translateX(0vw) translateY(0)";
+        blContent.style.width = "100vw";
+        blContent.style.height = "100vh";
+        blContent.style.top = "0";
+        blContent.style.display = "flex";
+        blContent.style.alignItems = "center";
+        blContent.style.justifyContent = "center";
+        blContent.style.background = "var(--bg-transparent)";
+        blContent.style.zIndex = "200";
+        trBtn.style.zIndex = "100";
+        tlBtn.style.zIndex = "100";
+        blBtn.style.zIndex = "300";
+        brBtn.style.zIndex = "100";
+        projectOne.style.width = "40%";
+        projectOne.style.margin = "auto auto 0.5rem";
+        projectTwo.style.width = "40%";
+        projectTwo.style.margin = "auto auto 0.5rem";
+        projectThree.style.width = "40%";
+        projectThree.style.margin = "auto auto 0.5rem";
+      } else {
+        blActive = "translateX(10vw) translateY(7vh)";
+        blContent.style.transform = "translateX(10vw) translateY(7vh)";
+        blContent.style.width = "15rem";
+        blContent.style.height = "0";
+        blContent.style.top = "40vh";
+        blContent.style.display = "block";
+        projectOne.style.width = "100%";
+        projectTwo.style.width = "100%";
+        projectThree.style.width = "100%";
+      }
+      break;
 
-    // case "buttom-right":
-    //   break;
+    case "bottom-right":
+      if (window.innerWidth <= 1100) {
+        brActive = "translateX(0) translateY(0)";
+        brContent.style.transform = "translateX(0vw) translateY(0)";
+        brContent.style.width = "100vw";
+        brContent.style.height = "100vh";
+        brContent.style.bottom = "0";
+        brContent.style.display = "flex";
+        brContent.style.flexDirection = "column";
+        brContent.style.alignItems = "center";
+        brContent.style.justifyContent = "center";
+        brContent.style.background = "var(--bg-transparent)";
+        brContent.style.zIndex = "200";
+        trBtn.style.zIndex = "100";
+        tlBtn.style.zIndex = "100";
+        blBtn.style.zIndex = "100";
+        brBtn.style.zIndex = "300";
+      } else {
+        brActive = "translateX(-5vw) translateY(0)";
+        brContent.style.transform = "translateX(-5vw) translateY(0)";
+        brContent.style.width = "30vw";
+        brContent.style.height = "0";
+        brContent.style.bottom = "30vh";
+        brContent.style.display = "block";
+      }
+      break;
 
     default:
   }
@@ -120,12 +191,10 @@ let lastReverseAnimation = "";
 
 // Play animation function
 function playAnimation(animation, reverseAnimation) {
-  // Remove all the animation classes form heroImage
+  // Remove all the animation classes from heroImage
   heroImage.className = "";
 
   if (lastReverseAnimation !== "") {
-    //clicked something
-    //reverse code here
     heroImage.classList.add(lastReverseAnimation);
     setTimeout(function () {
       heroImage.classList.remove(lastReverseAnimation);
@@ -133,7 +202,6 @@ function playAnimation(animation, reverseAnimation) {
       lastReverseAnimation = reverseAnimation;
     }, 200);
   } else {
-    //play forward code
     heroImage.classList.add(animation);
     lastReverseAnimation = reverseAnimation;
   }
@@ -142,8 +210,8 @@ function playAnimation(animation, reverseAnimation) {
 function playClosingAnimation(reverseAnimation) {
   tlBtn.innerHTML = "About";
   trBtn.innerHTML = "Experience";
-  // blBtn.innerHTML = "Projects";
-  // brBtn.innerHTML = "Contacts";
+  blBtn.innerHTML = "Projects";
+  brBtn.innerHTML = "Contact";
 
   switch (activeCorner) {
     case "top-left":
@@ -151,23 +219,21 @@ function playClosingAnimation(reverseAnimation) {
       tlBtn.style.color = textColor;
       tlContent.style.transform = tlHidden;
       break;
-
     case "top-right":
       trBtn.style.background = bgColor;
       trBtn.style.color = textColor;
       trContent.style.transform = trHidden;
       break;
-
-    // case "buttom-left":
-    //   tlBtn.style.background = bgColor;
-    //   tlBtn.style.color = textColor;
-    //   tlContent.style.transform = tlHidden;
-    //   break;
-    // case "buttom-right":
-    //   tlBtn.style.background = bgColor;
-    //   tlBtn.style.color = textColor;
-    //   tlContent.style.transform = tlHidden;
-    //   break;
+    case "bottom-left":
+      blBtn.style.background = bgColor;
+      blBtn.style.color = textColor;
+      blContent.style.transform = blHidden;
+      break;
+    case "bottom-right":
+      brBtn.style.background = bgColor;
+      brBtn.style.color = textColor;
+      brContent.style.transform = brHidden;
+      break;
 
     default:
   }
@@ -181,41 +247,38 @@ function playClosingAnimation(reverseAnimation) {
   }, 200);
 }
 
-// handle the onclick for each corner
-//Onclick corner button  functions
-
+// Onclick corner button functions
 tlBtn.onclick = function () {
   if (activeCorner === "top-left") {
     playClosingAnimation("reverse-animate-top-left");
   } else {
-    //removing arrow by default
     trBtn.innerHTML = "Experience";
-    // blBtn.innerHTML = "Projects";
-    // brBtn.innerHTML = "Contacts";
+    blBtn.innerHTML = "Projects";
+    brBtn.innerHTML = "Contact";
 
-    // setting active corner
+    // Setting activeCorner
     activeCorner = "top-left";
-    tlBtn.innerHTML = "&uarr; <br/> About"; //adding arrow above about
+    tlBtn.innerHTML = "&uarr;<br/>About";
 
     handleWindowResize();
     playAnimation("animate-top-left", "reverse-animate-top-left");
 
-    //change background colors
+    // Change background colors
     trBtn.style.background = bgColor;
-    // brBtn.style.background = bgColor;
-    // blBtn.style.background = bgColor;
+    brBtn.style.background = bgColor;
+    blBtn.style.background = bgColor;
     tlBtn.style.background = bgColorAlt;
 
-    //change text colors
+    // Change text colors
     trBtn.style.color = textColor;
-    // brBtn.style.color = textColor;
-    // blBtn.style.color = textColor;
+    brBtn.style.color = textColor;
+    blBtn.style.color = textColor;
     tlBtn.style.color = textColorAlt;
 
-    // Change the positoins of the corner content
+    // Change positions of the corner content
     trContent.style.transform = trHidden;
-    // brContent.style.transform = brHidden;
-    // blContent.style.transform = blHidden;
+    brContent.style.transform = brHidden;
+    blContent.style.transform = blHidden;
     tlContent.style.transform = tlActive;
   }
 };
@@ -225,8 +288,8 @@ trBtn.onclick = function () {
     playClosingAnimation("reverse-animate-top-right");
   } else {
     tlBtn.innerHTML = "About";
-    // blBtn.innerHTML = "Projects";
-    // brBtn.innerHTML = "Contact";
+    blBtn.innerHTML = "Projects";
+    brBtn.innerHTML = "Contact";
 
     // Setting activeCorner
     activeCorner = "top-right";
@@ -237,92 +300,90 @@ trBtn.onclick = function () {
 
     // Change background colors
     trBtn.style.background = bgColorAlt;
-    // brBtn.style.background = bgColor;
-    // blBtn.style.background = bgColor;
+    brBtn.style.background = bgColor;
+    blBtn.style.background = bgColor;
     tlBtn.style.background = bgColor;
 
     // Change text colors
     trBtn.style.color = textColorAlt;
-    // brBtn.style.color = textColor;
-    // blBtn.style.color = textColor;
+    brBtn.style.color = textColor;
+    blBtn.style.color = textColor;
     tlBtn.style.color = textColor;
 
     // Change positions of the corner content
     trContent.style.transform = trActive;
-    // brContent.style.transform = brHidden;
-    // blContent.style.transform = blHidden;
+    brContent.style.transform = brHidden;
+    blContent.style.transform = blHidden;
     tlContent.style.transform = tlHidden;
   }
 };
 
-// blBtn.onclick = function () {
-//     if(activeCorner === "buttom-left") {
-//         playClosingAnimation("reverse-animate-buttom-left");
-//     } else {
-//         //removing arrow by default
-//         tlBtn.innerHTML = "About";
-//         trBtn.innerHTML = "Experience";
-//         brBtn.innerHTML = "Contacts";
+blBtn.onclick = function () {
+  if (activeCorner === "bottom-left") {
+    playClosingAnimation("reverse-animate-bottom-left");
+  } else {
+    tlBtn.innerHTML = "About";
+    trBtn.innerHTML = "Experience";
+    brBtn.innerHTML = "Contact";
 
-//         //setting active corner
-//         activeCorner="buttom-left";
-//         blBtn.innerHTML = "Projects <br/> &darr;";   //adding arrow above about
+    // Setting activeCorner
+    activeCorner = "bottom-left";
+    blBtn.innerHTML = "Projects<br/>&darr;";
 
-//         handleWindowResize();
-//         playAnimation("animate-buttom-left", "reverse-animate-buttom-left");
+    handleWindowResize();
+    playAnimation("animate-bottom-left", "reverse-animate-bottom-left");
 
-//         //change background colors
-//         trBtn.style.background = bgColor;
-//         brBtn.style.background = bgColor;
-//         blBtn.style.background = bgColorAlt;
-//         tlBtn.style.background = bgColor;
+    // Change background colors
+    trBtn.style.background = bgColor;
+    brBtn.style.background = bgColor;
+    blBtn.style.background = bgColorAlt;
+    tlBtn.style.background = bgColor;
 
-//         //change text colors
-//         trBtn.style.color = textColor;
-//         brBtn.style.color = textColor;
-//         blBtn.style.color = textColorAlt;
-//         tlBtn.style.color = textColor;
+    // Change text colors
+    trBtn.style.color = textColor;
+    brBtn.style.color = textColor;
+    blBtn.style.color = textColorAlt;
+    tlBtn.style.color = textColor;
 
-//         // Change the positoins of the corner content
-//         trContent.style.transform = trHidden;
-//         brContent.style.transform = brHidden;
-//         blContent.style.transform = blActive;
-//         tlContent.style.transform = tlHidden;
-//     }
-// };
+    // Change positions of the corner content
+    trContent.style.transform = trHidden;
+    brContent.style.transform = brHidden;
+    blContent.style.transform = blActive;
+    tlContent.style.transform = tlHidden;
+  }
+};
 
-// brBtn.onclick = function () {
-//     if(activeCorner === "buttom-right") {
-//         playClosingAnimation("reverse-animate-buttom-right");
-//     } else {
-//         //removing arrow by default
-//         tlBtn.innerHTML = "About";
-//         trBtn.innerHTML = "Experience";
-//         blBtn.innerHTML = "Projects";
+brBtn.onclick = function () {
+  if (activeCorner === "bottom-right") {
+    playClosingAnimation("reverse-animate-bottom-right");
+  } else {
+    tlBtn.innerHTML = "About";
+    trBtn.innerHTML = "Experience";
+    blBtn.innerHTML = "Projects";
 
-//         //setting active corner
-//         activeCorner="buttom-right";
-//         brBtn.innerHTML = "Contacts <br/> &darr;";   //adding arrow above about
+    // Setting activeCorner
+    activeCorner = "bottom-right";
+    brBtn.innerHTML = "Contact<br/>&darr;";
 
-//         handleWindowResize();
-//         playAnimation("animate-buttom-right", "reverse-animate-buttom-right");
+    handleWindowResize();
+    playAnimation("animate-bottom-right", "reverse-animate-bottom-right");
 
-//         //change background colors
-//         trBtn.style.background = bgColor;
-//         brBtn.style.background = bgColorAlt;
-//         blBtn.style.background = bgColor;
-//         tlBtn.style.background = bgColor;
+    // Change background colors
+    trBtn.style.background = bgColor;
+    brBtn.style.background = bgColorAlt;
+    blBtn.style.background = bgColor;
+    tlBtn.style.background = bgColor;
 
-//         //change text colors
-//         trBtn.style.color = textColor;
-//         brBtn.style.color = textColorAlt;
-//         blBtn.style.color = textColor;
-//         tlBtn.style.color = textColor;
+    // Change text colors
+    trBtn.style.color = textColor;
+    brBtn.style.color = textColorAlt;
+    blBtn.style.color = textColor;
+    tlBtn.style.color = textColor;
 
-//         // Change the positoins of the corner content
-//         trContent.style.transform = trHidden;
-//         brContent.style.transform = brActive;
-//         blContent.style.transform = blHidden;
-//         tlContent.style.transform = tlHidden;
-//     }
-// };
+    // Change positions of the corner content
+    trContent.style.transform = trHidden;
+    brContent.style.transform = brActive;
+    blContent.style.transform = blHidden;
+    tlContent.style.transform = tlHidden;
+  }
+};
--- js/color-mode.js
+++ js/color-mode.js
@@ -1,46 +1,39 @@
 // Define DOM elements
-
-// #hero__animation WRONG WAY
-
-const toggleButton = document.querySelector("#toggle_button");
-// console.log(toggleButton);
-
+const toggleButton = document.querySelector("#toggle-button");
 const root = document.querySelector(":root");
 const storageKey = "color-mode";
 const defaultMode = "light-mode";
 
-//load the user's preferred color mode from local storage.
+// Load the user's preffered color mode from local storage.
 function loadColorMode() {
-    const colorMode = localStorage.getItem(storageKey);
-    root.classList.add(colorMode || defaultMode);
-    // updateToggleButton();
+  const colorMode = localStorage.getItem(storageKey);
+  root.classList.add(colorMode || defaultMode);
+  updateToggleButton();
 }
 
 loadColorMode();
 
-//Toggle the color mode
+// Toggle the color mode
 toggleButton.addEventListener("click", () => {
-    saveColorMode();
-})
+  saveColorMode();
+});
 
-//Save the user's preferred color mode to local storage
-function saveColorMode(){
-    //check if the root element has a class of "dark-mode". If yes, then the current mode is switched to light, and vice versa.
-    const currentMode = root.classList.contains("dark-mode") 
-    ? "light-mode" 
+// Save the users's preffered color mode to local storage
+function saveColorMode() {
+  // Check if the root element has a class of "dark-mode". If yes, then the current mode is switched to light, and vice versa.
+  const currentMode = root.classList.contains("dark-mode")
+    ? "light-mode"
     : "dark-mode";
-    root.classList.remove("light-mode", "dark-mode");
-    root.classList.add(currentMode);
-    localStorage.setItem(storageKey, currentMode);
-    // updateToggleButton();
+  root.classList.remove("light-mode", "dark-mode");
+  root.classList.add(currentMode);
+  localStorage.setItem(storageKey, currentMode);
+  updateToggleButton();
 }
 
-//removed redundant js function: update toggle button, already defined in css. confusion cleared"
-
-// function updateToggleButton() {
-//     if (root.classList.contains("dark-mode")){
-//         toggleButton.style.backgroundImage = "var(--moon)";
-//     } else{
-//         toggleButton.style.backgroundImage = "var(--sun)";
-//     }
-// }
+function updateToggleButton() {
+  if (root.classList.contains("dark-mode")) {
+    toggleButton.style.backgroundImage = "var(--moon)";
+  } else {
+    toggleButton.style.backgroundImage = "var(--sun)";
+  }
+}
